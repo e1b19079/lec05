25a905da941c1df3c986bd3ff9960d2678f5e830
e1b19099 <e1b19099@oit.ac.jp>
2021-10-26 16:26:14 +0900

    sample5-6-1

--- src/main/java/team18/lec05/Lec05Application.java
+++ src/main/java/team18/lec05/Lec05Application.java
@@ -2,12 +2,14 @@ package team18.lec05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Lec05Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lec05Application.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(Lec05Application.class, args);
+  }
 
 }
